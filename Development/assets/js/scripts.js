33065464a6b00cd22b9217d8dba91e9771b2390b
طيبة جمال العصفور -317 <taibahalasfour224@gmail.com>
2020-07-06 17:01:13 +0300

    dooone!!!

--- Development/assets/js/scripts.js
+++ Development/assets/js/scripts.js
@@ -1,23 +1,44 @@
 let arr = [];
 
-// Test
-console.log(arr);
+
+// let array =[
+//   "JavaScriptحب نانسي ل",
+//   "Reactاتقان استاذ مناور ل",
+//  "حماس موضي",
+//  "قدرات خاجة",
+//   "طيبة رهف",
+//    "شدة حسين",
+//    "  مساعدات سموالعجيبة",
+//   ];
+
+// // Test
+// console.log(arr);
 
 // onClick هذه الدالة المسؤولة عن إضافة العنصر في السلة عن طريق
-function addToCart() {
-  // أكمل
+function addToCart(item) {
+  arr.push(item);
 }
 
 let ul = document.createElement("ul");
 ul.className = "myUL";
 document.getElementById("container").appendChild(ul);
 
-// create(arr) هذه الدالة المسؤولة عن استدعاء
-function myFunction() {
-  // أكمل
-}
+
 
 // arr هذه الدالة المسؤولة عن إنشاء قائمة المشتريات من العناصر الموجودة في المصفوفة
 function create(arr) {
+  arr.forEach(function (data) {
+    let li = document.createElement("li");
+    ul.appendChild(li);
+    li.innerHTML += data;
+    li.className = "myList";
+  });
   // أكمل
+}
+
+// create(arr) هذه الدالة المسؤولة عن استدعاء
+
+function myarray() {
+  create(arr);
+  
 }
